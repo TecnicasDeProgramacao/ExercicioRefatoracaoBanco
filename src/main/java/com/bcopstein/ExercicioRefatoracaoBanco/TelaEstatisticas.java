9e300b117d30a8faa7be36c3cca629704de84560
KIN MAX PIAMOLINI GUSMAO <16104046@pucrs.br>
2018-11-06 11:13:04 -0200

    Início estatistica

---
+++ src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/TelaEstatisticas.java
@@ -0,0 +1,31 @@
+package com.bcopstein.ExercicioRefatoracaoBanco;
+
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class TelaEstatisticas 
+{
+	public TelaEstatisticas() 
+	{
+		
+	}
+}

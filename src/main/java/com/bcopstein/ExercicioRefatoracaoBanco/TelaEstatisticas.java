8c05ba3606c459d2022d5aca887a702bb52be965
Kin Gusmão <gusmaokin@gmail.com>
2018-11-18 05:44:15 -0200

    ETAPA 1 CONCLUIDA

--- src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/TelaEstatisticas.java
+++ src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/TelaEstatisticas.java
@@ -1,8 +1,13 @@
 package com.bcopstein.ExercicioRefatoracaoBanco;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.stream.Collectors;
+
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -15,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.DatePicker;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
@@ -39,6 +45,7 @@ public class TelaEstatisticas
 		this.operacoes = operacoes;
 	}
 	
+	@SuppressWarnings("restriction")
 	public Scene getTelaEstatisticas()
 	{
 		GridPane grid = new GridPane();
@@ -53,16 +60,114 @@ public class TelaEstatisticas
 		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
 		grid.add(scenetitle, 0, 0, 2, 1);
 		
-		double saldoDia = 0;
-		double saldoMes = 0;
+		DatePicker dp = new DatePicker();
+		
+		grid.add(dp, 2, 1);
+		
+		Label saldoMedioMes = new Label();
+		grid.add(saldoMedioMes, 2, 2);
+		
+		Label credits = new Label();
+		Label debits = new Label();
+		grid.add(credits, 2, 3);
+		grid.add(debits, 2, 4);
+		
 		GregorianCalendar date = new GregorianCalendar();
-		for(Operacao o: this.operacoes)
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+		LocalDate localDate = LocalDate.parse(date.get(GregorianCalendar.YEAR) + "-" + 
+											 (date.get(GregorianCalendar.MONTH)+1) + "-" + 
+											  date.get(GregorianCalendar.DAY_OF_MONTH), formatter);
+		
+		dp.setOnAction(e ->
 		{
-			if(o.getNumeroConta() == this.conta.getNumero() && o.getMes() == date.get(GregorianCalendar.MONTH)+1 && o.getAno() == date.get(GregorianCalendar.YEAR))
+			LocalDate ld = dp.getValue();
+			int mes = ld.getMonthValue();
+			int ano = ld.getYear();
+			ArrayList<Double> saldosDias = new ArrayList<Double>();
+			double totalMes = 0;
+			int day = 1;
+			double saldoDia = 0;
+			int creditos = 0, debitos = 0;
+			ArrayList<Operacao> opsMes = new ArrayList<Operacao>();
+			for(Operacao op: this.operacoes)
 			{
-				
+				if(op.getNumeroConta() == this.conta.getNumero())
+				{
+					if(op.getAno() < ano || op.getMes() < mes)
+					{
+						
+						if(op.getTipoOperacao() == 0) //CRÉDITO
+						{
+							saldoDia += op.getValorOperacao();
+						}
+						else //DÉBITO
+						{
+							saldoDia -= op.getValorOperacao();
+						}
+					}
+					else
+					{
+						opsMes.add(op);
+					}
+				}
 			}
-		}
+			
+			if(opsMes.isEmpty())
+			{
+				for(int i = 1; i < 31; i++)
+				{
+					saldosDias.add(saldoDia);
+				}
+			}
+			else
+			{
+				for(Operacao op: opsMes)
+				{
+					while(op.getDia() > day)
+					{
+						saldosDias.add(saldoDia);
+						day++;
+					}
+					
+					if(op.getTipoOperacao() == 0)
+					{
+						saldoDia += op.getValorOperacao();
+						creditos++;
+					}
+					else
+					{
+						saldoDia -= op.getValorOperacao();
+						debitos++;
+					}
+				}
+				while(saldosDias.size() < 30)
+				{
+					saldosDias.add(saldoDia);
+				}
+			}
+			for(Double d: saldosDias)
+			{
+				totalMes += d;
+			}
+			double media = totalMes/30.0;
+			saldoMedioMes.setText("Saldo Médio do Mês: " + media);
+			credits.setText("Créditos: " + creditos);
+			debits.setText("Débitos: " + debitos);
+			
+		});
+		
+		dp.setValue(localDate);
+		
+		Button btnVoltar = new Button("Voltar");
+		HBox hbBtn = new HBox(20);
+		hbBtn.setAlignment(Pos.TOP_CENTER);
+		hbBtn.getChildren().add(btnVoltar);
+		grid.add(hbBtn, 2, 5);
+		
+		btnVoltar.setOnAction(e ->
+		{
+			mainStage.setScene(cenaOperacoes);
+		});
 		
 		String categoria = "Categoria: "+conta.getStrStatus();
 		String limRetDiaria = "Limite retirada diaria: "+conta.getLimRetiradaDiaria();

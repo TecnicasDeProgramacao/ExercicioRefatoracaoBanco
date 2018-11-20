cf8e017bbb6cdacc26851ce482b915c0ad0769e5
LUCAS RIBEIRO DE ARAUJO <16104052@pucrs.br>
2018-11-20 11:22:07 -0200

    Operacoes Singleton

---
+++ src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/Operacoes.java
@@ -0,0 +1,59 @@
+package com.bcopstein.ExercicioRefatoracaoBanco;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Operacoes {
+
+	private static Operacoes instance;
+	private List<Operacao> operacoes;
+	
+	private Operacoes() 
+	{
+		this.operacoes = new LinkedList<Operacao>();
+	}
+
+	public static Operacoes getInstance() 
+	{
+		if(instance == null)
+		{
+			instance = new Operacoes();
+		}
+		
+		return (instance);
+	}
+	
+	public List<Operacao> operacaoPorConta(int numConta)
+	{
+		List<Operacao> opsSelecionadas = new LinkedList<Operacao>();
+		if(this.operacoes.size() <= 0) 
+		{
+			return opsSelecionadas;
+		}
+		
+		for(Operacao op : this.operacoes)
+		{
+			if(op.getNumeroConta() == numConta)
+			{
+				opsSelecionadas.add(op);
+			}
+		}		
+		return opsSelecionadas;
+	}
+	
+	public void loadOperacoes()
+	{
+		this.operacoes = Persistencia.getIntance().loadOperacoes();
+	}
+
+	public void addOperacao(Operacao op)
+	{
+		this.operacoes.add(op);
+	}
+	
+	public void saveOperacoes()
+	{
+		Persistencia.getIntance().saveOperacoes(this.operacoes);
+	}
+}

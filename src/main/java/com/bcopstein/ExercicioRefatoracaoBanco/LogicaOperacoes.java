a40ebf0c07a21972db836b79c79b7b0bf98ba139
Kin Gusmão <gusmaokin@gmail.com>
2018-11-24 17:36:45 -0200

    Arruma Logia Operacoes deleta Fachada

--- src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/LogicaOperacoes.java
+++ src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/LogicaOperacoes.java
@@ -11,46 +11,6 @@ public class LogicaOperacoes {
 	public LogicaOperacoes(){		
 	}
 	
-	public Boolean operacaoDebito(double valor, Conta conta)
-	{
-		if (valor < 0.0 || valor > conta.getSaldo()) 
-		{
-			//throw new NumberFormatException("Saldo insuficiente");
-			return false;
-		}
-		if(valor > conta.getLimRetiradaDiaria()) 
-		{
-			return false;
-		}
-		
-		double totalDoDia = valor;		
-		GregorianCalendar date = new GregorianCalendar();
-		totalDoDia += Operacoes.getInstance().somaValoresDiarios(conta.getNumero(), 1);; 
-		Operacoes.getInstance().loadOperacoes();		
-		
-		if(totalDoDia > conta.getLimRetiradaDiaria()) 
-		{
-			return false;
-		}
-		else
-		{
-			conta.retirada(valor);
-			Operacao op = new Operacao(
-					date.get(GregorianCalendar.DAY_OF_MONTH),
-					date.get(GregorianCalendar.MONTH)+1,
-					date.get(GregorianCalendar.YEAR),
-					date.get(GregorianCalendar.HOUR),
-					date.get(GregorianCalendar.MINUTE),
-					date.get(GregorianCalendar.SECOND),
-					conta.getNumero(),
-					conta.getStatus(),
-					valor,
-					1);
-			Operacoes.getInstance().addOperacao(op);
-			Operacoes.getInstance().saveOperacoes();
-			return true;
-		}		
-	}
 	
 	/*public Boolean operacaoCredito(double valor, Conta conta)
 	{
@@ -77,4 +37,6 @@ public class LogicaOperacoes {
 		operacoes.add(op);   
 		return true;
 	}*/
+	
+	
 }
--- src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/Operacoes.java
+++ src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/Operacoes.java
@@ -63,7 +63,7 @@ public class Operacoes {
 		this.loadOperacoes();
 		int totalDoDia = 0;
 		GregorianCalendar date = new GregorianCalendar();
-		for(Operacao o : operacoes) 
+		for(Operacao o : this.getListaOperacoes()) 
 		{					
 			if(o.getNumeroConta() == numConta && o.getDia() == date.get(GregorianCalendar.DAY_OF_MONTH)
 					&& o.getMes() == date.get(GregorianCalendar.MONTH)+1 && o.getAno() == date.get(GregorianCalendar.YEAR) && 
@@ -75,24 +75,88 @@ public class Operacoes {
 		return totalDoDia;
 	}
 	
-	public List<Operacao> getListaOperacoes()
+	public List<Operacao> getExtrato(int conta)
+	{
+		List<Operacao> opsConta = new LinkedList<Operacao>();
+		for(Operacao op: this.getListaOperacoes())
+		{
+			if(op.getNumeroConta() == conta)
+			{
+				opsConta.add(op);
+			}
+		}
+		return opsConta;
+	}
+	
+	public Boolean operacaoDebito(double valor, Conta conta)
+	{
+		if (valor < 0.0 || valor > conta.getSaldo()) 
+		{
+			//throw new NumberFormatException("Saldo insuficiente");
+			return false;
+		}
+		if(valor > conta.getLimRetiradaDiaria()) 
+		{
+			return false;
+		}
+		
+		double totalDoDia = valor;		
+		GregorianCalendar date = new GregorianCalendar();
+		totalDoDia += Operacoes.getInstance().somaValoresDiarios(conta.getNumero(), 1);; 
+		Operacoes.getInstance().loadOperacoes();		
+		
+		if(totalDoDia > conta.getLimRetiradaDiaria()) 
+		{
+			return false;
+		}
+		else
+		{
+			conta.retirada(valor);
+			Operacao op = new Operacao(
+					date.get(GregorianCalendar.DAY_OF_MONTH),
+					date.get(GregorianCalendar.MONTH)+1,
+					date.get(GregorianCalendar.YEAR),
+					date.get(GregorianCalendar.HOUR),
+					date.get(GregorianCalendar.MINUTE),
+					date.get(GregorianCalendar.SECOND),
+					conta.getNumero(),
+					conta.getStatus(),
+					valor,
+					1);
+			this.addOperacao(op);
+			this.saveOperacoes();
+			return true;
+		}	
+	} 
+	
+	/*public Boolean operacaoCredito(double valor, Conta conta)
+	{
+		
+		if (valor < 0.0) 
+		{
+			return false;
+		}
+		this.loadOperacoes();
+		conta.deposito(valor);
+		
+		GregorianCalendar date = new GregorianCalendar();
+		Operacao op = new Operacao(
+				date.get(GregorianCalendar.DAY_OF_MONTH),
+				date.get(GregorianCalendar.MONTH)+1,
+				date.get(GregorianCalendar.YEAR),
+				date.get(GregorianCalendar.HOUR),
+				date.get(GregorianCalendar.MINUTE),
+				date.get(GregorianCalendar.SECOND),
+				conta.getNumero(),
+				conta.getStatus(),
+				valor,
+				0);
+		operacoes.add(op);   
+		return true;
+	}*/
+	
+	private List<Operacao> getListaOperacoes()
 	{
 		return operacoes;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

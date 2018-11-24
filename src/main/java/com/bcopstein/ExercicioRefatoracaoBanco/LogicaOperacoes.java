ac68b18b9971aa2f6c23407832741fda4f7068fd
Kin Gusmão <gusmaokin@gmail.com>
2018-11-24 17:39:17 -0200

    Arruma LogiaOperacoes

--- src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/LogicaOperacoes.java
+++ src/main/java/com/bcopstein/ExercicioRefatoracaoBanco/LogicaOperacoes.java
@@ -12,31 +12,4 @@ public class LogicaOperacoes {
 	}
 	
 	
-	/*public Boolean operacaoCredito(double valor, Conta conta)
-	{
-		
-		if (valor < 0.0) 
-		{
-			return false;
-		}
-		this.loadOperacoes();
-		conta.deposito(valor);
-		
-		GregorianCalendar date = new GregorianCalendar();
-		Operacao op = new Operacao(
-				date.get(GregorianCalendar.DAY_OF_MONTH),
-				date.get(GregorianCalendar.MONTH)+1,
-				date.get(GregorianCalendar.YEAR),
-				date.get(GregorianCalendar.HOUR),
-				date.get(GregorianCalendar.MINUTE),
-				date.get(GregorianCalendar.SECOND),
-				conta.getNumero(),
-				conta.getStatus(),
-				valor,
-				0);
-		operacoes.add(op);   
-		return true;
-	}*/
-	
-	
 }

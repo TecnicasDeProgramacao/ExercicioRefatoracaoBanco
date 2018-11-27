41bac47dc01f6c064a67b035eaf68f28aaf75ab9
lucasribara <lucas.araujo.003@acad.pucrs.br>
2018-11-26 22:57:54 -0200

    testes start

---
+++ src/test/java/com/bcopstein/ExercicioRefatoracaoBanco/ContaTester.java
@@ -0,0 +1,14 @@
+package com.bcopstein.ExercicioRefatoracaoBanco;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContaTester {
+
+	@Test
+	public void test() {
+		assertEquals(1,1);
+	}
+
+}

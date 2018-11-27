dd307163c69cc1a92cb7675ec2a41e1c7f789cf0
KIN MAX PIAMOLINI GUSMAO <16104046@pucrs.br>
2018-11-27 09:27:09 -0200

    Inicio ContaTests

--- src/test/java/com/bcopstein/ExercicioRefatoracaoBanco/ContaTests.java
+++ src/test/java/com/bcopstein/ExercicioRefatoracaoBanco/ContaTests.java
@@ -1,17 +1,18 @@
 package com.bcopstein.ExercicioRefatoracaoBanco;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class ContaTester {
+public class ContaTests {
 
 	private Conta conta;
-	private Status mockStatus = mock(Status.class); 
+	private Status mockStatus = mock(Status.class);
 	
 	@BeforeEach
 	public void setUp() throws Exception {
@@ -32,9 +33,27 @@ public class ContaTester {
 	}
 	
 	@Test
-	public void testGetStatus()
+	public void testGetStrStatus()
 	{
 		conta = new Conta(1, "umNome");
 		assertEquals("Silver", conta.getStrStatus());
+	}
+	
+	@Test
+	public void testGetStatus()
+	{
+		conta = new Conta(1, "umNome");
+		assertEquals("Silver", conta.getStatus().getStrStatus());
+	}
+	
+	public void testGetLimRetiradaDiaria()
+	{
+		Conta conta1 = new Conta(1,"Joao",1200.0,new Silver());
+		Conta conta2 = new Conta(1,"Joao",52000.0,new Gold());
+		Conta conta3 = new Conta(1,"Joao",350000.0,new Platinum());
+		
+		assertEquals(5000, conta1.getLimRetiradaDiaria());
+		assertEquals(50000, conta2.getLimRetiradaDiaria());
+		assertEquals(500000, conta3.getLimRetiradaDiaria());
 	}
 }
